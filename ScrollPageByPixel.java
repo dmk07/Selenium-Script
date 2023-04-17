ef3ac0af7f26d4cc2f246404a58dff68da883101
Dipak Kadam <dipak100kadam@gmail.com>
2023-04-17 23:42:52 +0530

    Add files via upload

---
+++ ScrollPageByPixel.java
@@ -0,0 +1,42 @@
+package demos;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ScrollPageByPixel 
+{
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("https://cleartrip.com");
+		
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		
+		
+		js.executeScript("window.scrollBy(0,1000)");
+		driver.findElement(By.id("creditShell")).click();
+		
+		
+		
+	}
+	
+}
